0f4f723c911b308320f75988fa96a7d1fd415737
Raghav Nagpal <raghavnagpal@Raghavs-MacBook.local>
2024-01-03 23:08:47 +0530

    intial commit for command input

--- CommandInput.ts
+++ CommandInput.ts
@@ -1,1 +1,16 @@
 // this is the component for the input bar of the command input.
+
+// CommandInput.ts
+
+class CommandInput {
+    private placeholder: string;
+    private iconPath: string;
+    constructor(placeholder: string, iconPath: string) {
+        this.placeholder = placeholder;
+        this.iconPath = iconPath;
+    }
+
+    
+}
+
+export default CommandInput;
