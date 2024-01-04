0e6309f63614ec4123a1a6fb35f1549b4669c56f
Raghav Nagpal <raghavnagpal2003@gmail.com>
2024-01-04 23:40:47 +0530

    main file creation

---
+++ app.ts
@@ -0,0 +1,5 @@
+// app.ts
+import CommandInput from './CommandInput';
+
+const commandInput = new CommandInput("search more","svg path later");
+commandInput.render('container-id'); // Replace 'container-id' with the actual ID of your container div
