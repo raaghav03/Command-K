27bd73a7a178223bb59324d46acb3bc83341e956
Raghav Nagpal <raghavnagpal2003@gmail.com>
2024-01-05 23:50:41 +0530

    icon dependencies

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 // app.ts
 import CommandInput from './CommandInput';
 
-const commandInput = new CommandInput("search more","svg path later");
+const commandInput = new CommandInput("search more","<Search />");
 commandInput.render('container-id'); // Replace 'container-id' with the actual ID of your container div
